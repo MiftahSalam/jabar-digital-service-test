1f580876ffaaa902d6030e561b5836a76b5adb0a
Miftah Salam <salam.miftah@gmail.com>
2022-06-16 14:08:13 +0700

    add user logged in response handle and test

--- users/serializer/user.go
+++ users/serializer/user.go
@@ -1,6 +1,7 @@
 package serializer
 
 import (
+	"github.com/MiftahSalam/jabar-digital-service-test/commons"
 	"github.com/MiftahSalam/jabar-digital-service-test/users/model"
 	"github.com/gin-gonic/gin"
 )
@@ -17,11 +18,32 @@ type UserRegisteredResponse struct {
 	Password string `json:"password"`
 }
 
+type UserLoggedInSerializer struct {
+	Ctx  *gin.Context
+	User *model.User
+}
+
+type UserLoggedInResponse struct {
+	ID       uint   `json:"id"`
+	Username string `json:"username"`
+	Token    string `json:"token"`
+}
+
 func (user *UserRegisteredSerializer) Response() UserRegisteredResponse {
 	response := UserRegisteredResponse{
 		Username: user.User.Username,
 		Password: user.Password,
 		Role:     user.User.UserRole.Name,
+	}
+
+	return response
+}
+
+func (user *UserLoggedInSerializer) Response() UserLoggedInResponse {
+	response := UserLoggedInResponse{
+		Username: user.User.Username,
+		ID:       user.User.ID,
+		Token:    commons.GenerateToken(user.User.ID, user.User.Username),
 	}
 
 	return response
--- users/serializer/user_serializer_test.go
+++ users/serializer/user_serializer_test.go
@@ -1,10 +1,13 @@
 package serializer
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/MiftahSalam/jabar-digital-service-test/users/model"
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
 func TestUserRegisteredResponse(t *testing.T) {
@@ -23,4 +26,29 @@ func TestUserRegisteredResponse(t *testing.T) {
 	asserts.Equal(serializer.User.Username, response.Username)
 	asserts.Equal(serializer.User.UserRole.Name, response.Role)
 	asserts.Equal(serializer.Password, response.Password)
+}
+
+func TestUserLoggedInResponse(t *testing.T) {
+	asserts := assert.New(t)
+
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		panic("Cannot load environment file")
+	}
+
+	serializer := UserLoggedInSerializer{
+		Ctx: nil,
+		User: &model.User{
+			Username: "miftah",
+			Model:    gorm.Model{ID: 20},
+			UserRole: model.Role{Name: "User"},
+		},
+	}
+	response := serializer.Response()
+
+	fmt.Println("response", response)
+
+	asserts.Equal(serializer.User.Username, response.Username)
+	asserts.Equal(serializer.User.ID, response.ID)
+	asserts.NotEmpty(response.Token)
 }
