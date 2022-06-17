e651fc53dc998bf093ceca357b7ea76cdb619aee
Miftah Salam <salam.miftah@gmail.com>
2022-06-17 11:38:35 +0700

    First run implementation

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/MiftahSalam/jabar-digital-service-test/commons/database"
+	"github.com/MiftahSalam/jabar-digital-service-test/users/model"
+	userRouter "github.com/MiftahSalam/jabar-digital-service-test/users/router"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	production_mode := os.Getenv("PRODUCTION")
+	if production_mode == "" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			fmt.Println("Error while loading env file with error", err)
+			panic("Error while loading env file")
+		}
+	}
+
+	err := database.InitDatabase()
+	if err != nil {
+		fmt.Println("Error while initialize database with error", err)
+		panic("Error while initialize database")
+	}
+
+	model.Migrate() //migrate user models
+
+	router := gin.Default()
+	v1 := router.Group("/api/v1")
+	userRouter.Users(v1.Group("/auth"))
+
+	router.Run()
 }
